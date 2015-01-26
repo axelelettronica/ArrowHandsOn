e0ae8abfcbb4ecd36d42ee98bedc7a769ea570f3
Severino Peirano <severino.peirano@gmail.com>
2015-01-26 15:52:05 +0100

    Replace gps commit

---
+++ src/sme/tasks/sme_gps_task.h
@@ -0,0 +1,26 @@
+/*
+ * sme_gps_controller.h
+ *
+ * Created: 22/01/2015 17:21:20
+ *  Author: speirano
+ */ 
+#ifndef SME_GPS_TASK_H_
+#define SME_GPS_TASK_H_
+
+//#include <stdint-gcc.h>
+#include "../sme_FreeRTOS.h"
+
+#define SME_GPS_MAX_DATA_LEN             82
+#define GPS_TASK_NAME (const char *)   "GPS"
+
+
+//! semaphore for incoming execution command
+
+extern xSemaphoreHandle gps_sem;
+
+
+
+BaseType_t sme_gps_mgr_init(void);
+
+
+#endif /* SME_GPS_CONTROLLER_H_ */
